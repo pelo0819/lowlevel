f9b3b78a9b1bdc3f414ea3d39f9183a52d1fd3e6
pelo0819 <canvas.pelo@gmail.com>
2021-10-10 17:57:21 +0900

    update linker

--- assembly/elf_analyse/targets/c/linker/linklib.c
+++ assembly/elf_analyse/targets/c/linker/linklib.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <elf.h>
 
 #include "linklib.h"
 
@@ -65,7 +61,7 @@ int search_symbol(struct obj objs[], char *name, struct obj *obj)
 {
     int i;
     int n;
-    void *head;
+    char *head;
     Elf64_Ehdr *ehdr;
     Elf64_Shdr *shdr;
     Elf64_Shdr *symtab;
@@ -84,30 +80,222 @@ int search_symbol(struct obj objs[], char *name, struct obj *obj)
         // symtabがシンボルテーブルのセクションか確認
         if(symtab->sh_entsize != sizeof(Elf64_Sym))
         {
-            printf("[!!!] invalid entry size. (%d)\n", symtab->sh_entsize);
-            return -1;
+            printf("[!!!] invalid entry size. (%ld)\n", symtab->sh_entsize);
+            return 1;
         }
 
-        for(i = 0; i < symtab->)
+        // .symtabセクションの場合、sh_entsizeの大きさの配列状のデータ構造であるので
+        // .symtabセクションが終わるまで回して、各シンボル情報に対して解析を行う
+        for(i = 0; i < symtab->sh_size; i += symtab->sh_entsize)
+        {
+            symp = (Elf64_Sym *)(head + symtab->sh_offset + i);
+            if(!is_symbol_table(symp))
+            { 
+                continue; 
+            }
 
+            // シンボル名が格納されているポインタをセット
+            char *sym_name = head + strtab->sh_offset + symp->st_name;
+            if(!strcmp(name, sym_name))
+            {
+                // nameとsym_nameが一致したら、ヒットしたオブジェクトファイル名と
+                // シンボルのアドレスをセット
+                obj->filename = objs[n].filename;
+                obj->address = (char *)(head + shdr->sh_offset + symp->st_value);
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
 
+/*
+引数で与えたsympがシンボルテーブルか確認する
+シンボルテーブルなら0を返す
+*/
+int is_symbol_table(Elf64_Sym *symp)
+{
+    int falsE = 1;
+    // セクション名が格納されるアドレスを示すオフセットが0ならfalse
+    if(!symp->st_name)
+    { 
+        return falsE;
+    }
+    
+    if(symp->st_shndx == SHN_UNDEF || symp->st_shndx == SHN_ABS)
+    {
+        return falsE;
+    }
+    
+    if(ELF64_ST_BIND(symp->st_info) != STB_GLOBAL)
+    {
+        return falsE;
     }
 
+    if(get_sym_info_high(symp->st_info) != STB_GLOBAL)
+    {
+        return falsE;
+    }
 
-    return 0;
+    if(get_sym_info_low(symp->st_info) != STT_NOTYPE ||
+       get_sym_info_low(symp->st_info) != STT_OBJECT ||
+       get_sym_info_low(symp->st_info) != STT_FUNC)
+    {
+        return falsE;
+    }
+
+    return 0; // true
+}
+
+u_int8_t get_sym_info_high(u_int8_t info)
+{
+    return info >> 4;
+}
+
+u_int8_t get_sym_info_low(u_int8_t info)
+{
+    return info << 4;
+
 }
 
 int relocate_common_symbol(Elf64_Ehdr *ehdr)
 {
+    int i;
+    char *head;
+    Elf64_Shdr *symtab;
+    Elf64_Shdr *bss;
+    Elf64_Sym *symp;
+
+    symtab = get_section(ehdr, ".symtab");
+    bss = get_section(ehdr, ".bss");
+    head = (char *)ehdr;
+
+    // symtabがシンボルテーブルのセクションか確認
+    if(symtab->sh_entsize != sizeof(Elf64_Sym))
+    {
+        printf("[!!!] invalid entry size. (%ld)\n", symtab->sh_entsize);
+        return 1;
+    }
+
+    int size_bss_sec = bss->sh_size;
+    // .symtabセクションの場合、sh_entsizeの大きさの配列状のデータ構造であるので
+    // .symtabセクションが終わるまで回して、各シンボル情報に対して解析を行う
+    for(i = 0; i < symtab->sh_size; i += symtab->sh_entsize)
+    {
+        symp = (Elf64_Sym *)(head + symtab->sh_offset + i);
+        if(symp->st_shndx != SHN_COMMON)
+        {
+            continue;
+        }
+        int align = symp->st_value - 1;
+        size_bss_sec = (size_bss_sec + align) & ~align;
+        // ↓ 違くない？
+        symp->st_shndx = bss - (Elf64_Shdr *)(head + ehdr->e_shoff);
+        symp->st_value = size_bss_sec;
+        size_bss_sec += symp->st_size;
+    }
+
+    // 16bitでアラインメント
+    size_bss_sec = (size_bss_sec + 15) & ~15;
+    bss->sh_size = size_bss_sec;
     return 0;
 }
 
 int check_ehdr(Elf64_Ehdr *ehdr)
 {
-    return 0;
+    if(ehdr->e_ident[1] == 0x45 &&
+       ehdr->e_ident[2] == 0x4c &&
+       ehdr->e_ident[3] == 0x46)
+    {
+        return 0;
+    }
+    return 1;
+}
+
+static int link_symbol(
+    struct obj objs[], 
+    int objnum, 
+    Elf64_Ehdr *ehdr,
+    Elf64_Shdr *reltab,
+    Elf64_Shdr *defsec, // 再配置テーブルをもつセクション
+    Elf64_Shdr *symtab,
+    Elf64_Shdr *strtab)
+{
+    int i;
+    int addr;
+    int addend;
+    char *head;
+    unsigned int *addp;
+    Elf64_Shdr *shdr;
+    Elf64_Sym *symp;
+    Elf64_Rel *relp;
+    struct obj obj;
+
+    // Elfファイルの先頭アドレス
+    head = (char *)ehdr;
+    
+    // 再配置テーブルは配列状になっているので、配列分回す
+    for(int i = 0; i < reltab->sh_size; i += reltab->sh_entsize)
+    {
+        // 再配置テーブルの各要素を取得する
+        relp = (Elf64_Rel *)(head + reltab->sh_offset + i);
+        
+        // r_infoの上位24bitの情報(通し番号)に準じて
+        // シンボルテーブルからシンボル情報を取得する
+        symp = (Elf64_Sym *)(head + symtab->sh_offset + 
+            (symtab->sh_entsize * ELF64_R_SYM(relp->r_info)));
+        
+        printf("%-10s ", objs[objnum].filename);
+
+        if(symp->st_name)
+        {
+            // シンボル名を取得
+            printf("%-12s ", head + strtab->sh_offset + symp->st_name);
+        }
+    }
 }
 
 int link_objs(struct obj objs[])
 {
+    int i;
+    int j;
+    char *name;
+    Elf64_Ehdr *ehdr;
+    Elf64_Shdr *reltab;
+    Elf64_Shdr *defsec;
+    Elf64_Shdr *symtab;
+    Elf64_Shdr *strtab;
+
+    printf("\nfilename  symbol  found  type addend  called defined  rel type\n");
+    printf("---------- ------------ ---------- ---- -------- -------- -------- ------------\n");
+
+    // オブジェクトファイル分回す
+    for(i = 0; objs[i].filename; i++)
+    {
+        ehdr = (Elf64_Ehdr *)objs[i].address;
+        symtab = get_section(ehdr, ".symtab");
+        strtab = get_section(ehdr, ".strtab");
+
+        // シンボルテーブルが格納されているセクション、または、
+        // セクション名が格納されているセクションのどちらかがnullだったら、
+        // このオブジェクトファイルに対するリンクは行わない
+        if(!symtab || !strtab)
+        {
+            continue;
+        }
+
+        // 再配置テーブルを見るけるまで回す
+        for(j = 0; j < ehdr->e_shnum; j++)
+        {
+            reltab = (Elf64_Shdr *)((char *)ehdr + ehdr->e_shoff + ehdr->e_shentsize * j);
+            if(reltab->sh_type == SHT_REL || reltab->sh_type == SHT_RELA)
+            {
+                // 再配置テーブルが見つかったら、頭に.をつけて、セクションヘッダ(defsec)を取得
+                name = get_section_name(ehdr, reltab);
+                defsec = get_section(ehdr, strchr(name + 1, '.'));
+                link_symbol(objs, i, ehdr, reltab, defsec, symtab, strtab);
+            }
+        }
+    }
     return 0;
 }
